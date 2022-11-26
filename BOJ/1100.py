ad60462f7d876ffda688054a8cae1b7feaa091f3
GH-Jo <lkj004124@gmail.com>
2022-11-26 15:24:08 +0900

    Update 1100.py

--- BOJ/1100.py
+++ BOJ/1100.py
@@ -1,12 +1,11 @@
-status = []
 cnt = 0
-for _ in range(8):
-    status.append(list(input()))
-
 for i in range(8):
-    for j in range(8):
-        if (i%2 == 0) and (j%2 == 0):
-            cnt += int(status[i][j] == 'F')
-        elif (i%2 == 1) and (j%2 == 1):
-            cnt += int(status[i][j] == 'F')
+    if i % 2 == 0:
+        init = 0
+    else:
+        init = 1
+    line = list(input())
+    for j in range(init, 8, 2):
+        cnt += int(line[j] == 'F')
 print(cnt)
+         
