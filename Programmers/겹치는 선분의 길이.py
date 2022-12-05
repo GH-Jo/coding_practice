bb7cce1d9fd02b47ba63bfb7ccc3dd54d8d66608
GH-Jo <lkj004124@gmail.com>
2022-12-05 12:22:55 +0900

    프로그래머스 겹치는 선분의 길이

---
+++ Programmers/겹치는 선분의 길이.py
@@ -0,0 +1,13 @@
+def solution(lines):
+    dic = dict()
+    for line in lines:
+        for i in range(line[0], line[1]):
+            if dic.get(i) == None:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+    cnt = 0
+    for k,v in dic.items():
+        if v > 1:
+            cnt += 1
+    return cnt
